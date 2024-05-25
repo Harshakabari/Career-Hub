a8d6811ffdbbd0f0e48d8132e0af303b95ba3b59
khushprajapati1024 <khushprajapati777@gmail.com>
2024-05-25 14:18:55 +0530

    frontend

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Logo from "../../assets/logo.png";
+
+const Header = () => {
+  return (
+    <>
+      <div className="py-4 px-10   flex justify-between items-center">
+        <a href="/">
+          <img className="h-12" src={Logo} alt="" />
+        </a>
+
+        <nav>
+          <ul className="flex gap-6">
+            <li><a href="home">Home</a></li>
+            <li><a href="about">About Us</a></li>
+            <li><a href="postjob">Post Job</a></li>
+            <li><a href="findjob">Find Job</a></li>
+          </ul>
+        </nav>
+
+        <button className="bg-blue-700 rounded-md p-1 px-3" type="button">Login/Register</button>
+      </div>
+
+
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+
+const Home = () => {
+  return (
+    <div className='bg-blue-950 text-white h-dvh w-dvw'>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
