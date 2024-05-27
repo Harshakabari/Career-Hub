36b23561eafdc7463882df72776a2d8010bd2a72
khushprajapati1024 <khushprajapati777@gmail.com>
2024-05-27 15:25:32 +0530

    add sign up page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Logo from "../../assets/logo.png";
 import { RiMenu3Fill } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
   return (
@@ -19,7 +21,7 @@ const Header = () => {
           </ul>
         </nav>
 
-        <button className="bg-[#2277AE] rounded-md p-1 px-4 hidden md:block" type="button">Login/Register</button>
+        <button className="bg-[#2277AE] rounded-md p-1 px-4 hidden md:block" type="button"><Link to="/login">Login/Register</Link></button>
         <RiMenu3Fill class="block md:hidden text-3xl "/>
 
       </div>
