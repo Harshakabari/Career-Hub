4c7d300bf10f01bc35301f180295846c5e25220f
jaypatidar1230 <154257701+jaypatidar1230@users.noreply.github.com>
2024-06-06 17:06:32 +0530

    re

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 const Main = () => {
   return (
     <>
-      <div className="relative">
+      <div className="relative -mt-10 lg:mt-0 md:mt-0">
         {/* Background Animation */}
         <div className="area absolute inset-0 mt-[-16px]">
           <ul className="circles">
@@ -28,7 +28,7 @@ const Main = () => {
         {/* ================== content ================== */}
         <div className='text-white'>
         <img className='mx-auto my-1 h-80 w-[550px] pt-20' src={job} alt="" />
-         <h2 className='text-6xl text-center '> Get Your Dream Job <br /> with 
+         <h2 className='lg:text-6xl md:text-4xl text-3xl text-center '> Get Your Dream Job <br /> with 
          <span className='text-blue-500/100 '> Jobmaster</span> 
          </h2> 
         <div className='flex justify-center items-center py-6 gap-4'> 
@@ -40,16 +40,16 @@ const Main = () => {
 
         {/*=========== Search bar ============ */}
         <div className="text-black p-4 rounded-lg mt-10 w-max bg-slate-200 m-auto relative z-10">
-          <Form className="flex items-center gap-4 h-[50px]">
+          <Form className="grid lg:grid-cols-3 md:grid-cols-2 -mt-3 items-center justify-center gap-4 lg:h-[50px]">
             <FormGroup className="flex gap-3 items-center form__group form__group-fast">
               <IoSearch className='mt-3 ml-[10px] mr-[-50px] z-10 border-transparent bg-slate-300 rounded-lg border-2 h-8 w-8 p-[7px]' />
               <input className='p-2 mt-3 px-3 pl-[45px] rounded-md outline-none' type="text" placeholder="Job title or keyword" required />
             </FormGroup>
-            <FormGroup className="flex mt-3 gap-3 items-center form__group form__group-fast">
+            <FormGroup className="flex mt-3 gap-3 ml-2 lg:ml-0 md:ml-0 items-center form__group form__group-fast">
               <IoLocationOutline className='mr-[-50px] z-10 border-transparent bg-slate-300 rounded-lg border-2 h-8 w-8 p-[7px]'/>
               <input className='p-2 px-3 pl-[45px] rounded-md outline-none' type="text" placeholder="Location" />
             </FormGroup>
-            <button className="items-center px-10 py-2.5 text-sm font-medium text-center text-white bg-[#2277AE] rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800" type="submit">
+            <button className="items-center md:col-span-2 lg:col-span-1 px-10 py-2.5 text-sm font-medium text-center text-white bg-[#2277AE] rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800" type="submit">
               Search
             </button>
           </Form>
