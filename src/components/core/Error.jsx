ba38a3bb3b6fcabd8cbe103cb5585970d7d48356
Harshakabari <harshakabariedu@gmail.com>
2024-06-17 19:31:23 +0530

    error add

--- src/components/core/Error.jsx
+++ src/components/core/Error.jsx
@@ -1,7 +1,11 @@
+import error from "../../assets/error.png"
+
+
 function Error() {
   return (
-    <div className="flex flex-1 justify-center items-center text-white text-3xl">
-      Error 404 - Page Not Found
+      <div className="flex flex-col justify-center items-center">
+      <img src={error} alt="" className="w-[60rem] h-[500px] mix-blend-multiply	" />
+      <p className="text-blue-900 font-semibold font-serif text-4xl ">Error 404 - Page Not Found !!</p>
     </div>
   );
 }
--- src/pages/content.jsx
+++ src/pages/content.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import jobdesc from "../assets/mainbg.jpg";
-import { LuArrowBigRightDash } from "react-icons/lu";
+import { FaArrowCircleRight } from "react-icons/fa";
 import Blob from "../assets/blob.png";
 
 
@@ -8,10 +8,10 @@ import Blob from "../assets/blob.png";
 const content = () => {
   return (
     <>
-      <div className="grid lg:grid-cols-2 md:grid-cols-1 lg:h-[550px] h-[1200px] lg:mt-0 -mt-40 lg:gap-8 lg:px-32 items-center relative">
+      <div className="grid lg:grid-cols-2 md:grid-cols-1 lg:h-[550px] h-[1200px] lg:mt-0 -lg:gap-8 lg:px-32 items-center relative">
         <div>
           <img
-            className=" lg:h-[570px] w-[570px] mix-blend-multiply"
+            className=" lg:h-[550px] w-[570px] mix-blend-multiply"
             src={jobdesc}
             alt="jobdesc-image"
           />
@@ -31,21 +31,21 @@ const content = () => {
             </h3>
             <ul className="text-lg text-gray-700 py-6 leading-8 tracking-wide	-top-36">
               <li className="flex ">
-                <LuArrowBigRightDash className="w-[32px] text-2xl mt-1" />
+                <FaArrowCircleRight  className="w-[32px] text-2xl mt-1" />
                 Don't let work schedule run your life.
               </li>
               <li className="flex ">
-                <LuArrowBigRightDash className="w-[50px] text-2xl mt-1" />
+                <FaArrowCircleRight  className="w-[50px] text-2xl mt-1" />
                 We want to help you reach your full potential. Build new
                 experiences by working in different industries.
               </li>
               <li className="flex ">
-                <LuArrowBigRightDash className="w-[50px] text-2xl mt-1" /> We
+                <FaArrowCircleRight  className="w-[50px] text-2xl mt-1" /> We
                 want you to take back control of your time, Manage expectations
                 for that perfect work-life balance.
               </li>
               <li className="flex ">
-                <LuArrowBigRightDash className="w-20 text-2xl mt-1" />
+                <FaArrowCircleRight  className="w-20 text-2xl mt-1" />
                 We connect you with jobs allowing you to work where you want,
                 when you want, Every completed shift improves our matching,
                 ensuring we find you work that you enjoy.
